f1ef79b60f8ba0ff109791725849e3f56b4fd5f6
sixtwosix <hencostrauss@gmail.com>
2025-01-12 11:52:52 +0200

    Challenge 1 complete with Depth first search

--- Day10/Python/Challenge1.py
+++ Day10/Python/Challenge1.py
@@ -15,59 +15,42 @@ def DFS(row_start,col_start,grid,goal_val):
     goal_location = set()
 
     stack = []
-    visited = [[False for i in range(COL_MAX)] for j in range(ROW_MAX)]
+    visited = []
     stack.append([row_start,col_start])
     prev_val = -1
-    previous = [row_start,col_start-1]
     found_goal = False
     # Iterate untill the stack is empty
     while (len(stack) > 0):
         current = stack.pop()        
         row = current[0]
         col = current[1]
-        
-
-        # if location is invalid
-        if(col < 0) or (col >= COL_MAX) or (row < 0) or (row >= ROW_MAX):
-            continue
 
         # already visited
-        if(visited[row][col]) or (grid[row][col] == '.'):
+        if((row,col) in visited) or (grid[row][col] == '.'):
             continue        
         
         
         current_val = int(grid[row][col])
-        if(found_goal):
+        if(found_goal and current_val-prev_val != 1):
             prev_val = current_val -1
-            previous = [row,col-1]
             found_goal = False
 
-
-        neighbours = []
-        for i in range(4):
-            adjx = row + dRow[i]
-            adjy = col + dCol[i]
-            neighbours.append([adjx,adjy])
-
-        if(current_val != prev_val + 1) or ( previous not in neighbours):
-            continue
-        
-        prev_val = current_val
-        previous = current
-
-        visited[row][col] = True
+        visited.append((row,col))
         
         if(current_val == goal_val) and ((row,col) not in goal_location):
             goal_location.add((row,col))
             found_goal = True
-            print(f"Found a {goal_val} at location: {row} {col}")    
+            # print(f"Found a {goal_val} at location: {row} {col}")    
+        else:
+            prev_val = current_val
 
-        # print(grid[row][col], end = " ")
-
-        for i in range(4):
-            adjx = row + dRow[i]
-            adjy = col + dCol[i]
-            stack.append([adjx,adjy])
+            for i in range(4):
+                adjx = row + dRow[i]
+                adjy = col + dCol[i]
+                # check if next possible cells are valid
+                if(adjy >= 0) and (adjy < COL_MAX) and (adjx >= 0) and (adjx < ROW_MAX):
+                    if(grid[adjx][adjy] != '.') and (int(grid[adjx][adjy]) == prev_val + 1):
+                        stack.append([adjx,adjy])
         
         
     
@@ -75,7 +58,7 @@ def DFS(row_start,col_start,grid,goal_val):
 
 
 if __name__ == "__main__":
-    input_file = "test_input5.csv"
+    input_file = "input1.csv"
 
     grid = []
 
@@ -94,12 +77,11 @@ if __name__ == "__main__":
     trailhead_scores = []
 
     for start in starting_locations:
-        print(start)
+        # print(start)
         trailhead_scores.append(len(DFS(start[0],start[1],grid,9)))
-        
-        
 
-    print(trailhead_scores)
-    # print(f"Found this many 9s: {len(final_locations)}")
+    # print(trailhead_scores)
+    print(sum(trailhead_scores))
+
 
     

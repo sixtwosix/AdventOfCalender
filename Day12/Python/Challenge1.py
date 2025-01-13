f9b8670b9f5197f3eff71d94d6d42e06b114b643
Henco Strauss <henco.strauss@dyna-mo.com>
2025-01-13 13:17:35 +0200

    Comments of how to approach problem

---
+++ Day12/Python/Challenge1.py
@@ -0,0 +1,10 @@
+
+
+# use BFS to find each similar object of letter in list
+
+# count each letter 'X' for the area
+
+# count each letter 'X' that has a non 'X' letter next to it
+# thus have a 'Y' next to 'X'
+
+# check only directions vertical and horizontal

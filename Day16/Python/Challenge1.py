d14801c7561718d2edd9ddc818f50e4cac3a49db
Henco Strauss <henco.strauss@dyna-mo.com>
2025-04-14 13:17:48 +0200

    Added changes to day 16 challenge 1 to visualize better

--- Day16/Python/Challenge1.py
+++ Day16/Python/Challenge1.py
@@ -1,4 +1,5 @@
 from heapq import heapify, heappop, heappush
+import copy
 
 # node = (x,y,direction)
 class Graph:
@@ -9,41 +10,10 @@ class Graph:
         self.Y_MAX = len(maze_grid)
         self.X_MAX = len(maze_grid[0])
         
-        # dx = (0,1,0,-1)
-        # dy = (1,0,-1,0)
-        
-        # self.start_node = (-1,-1,1)
-        # self.end_node = (-1,-1,1)
-        
         for y in range(self.Y_MAX):
             for x in range(self.X_MAX):
                 if(maze_grid[y][x] == "S") or (maze_grid[y][x] == "E") or (maze_grid[y][x] == "."):
-                    self.graph[(x,y)] = {}    
-            
-        # queue = [self.start_node]
-        # visited = set()
-        
-        # while queue:
-        #     # node = (x,y,dir)
-
-        #     curr_node = queue.pop(0)
-        #     curr_x = curr_node[0]
-        #     curr_y = curr_node[1]
-        #     curr_dir = curr_node[2]
-        #     visited.add(curr_node)
-            
-        #     for i in range(4):
-        #         new_x = curr_x + dx[i]
-        #         new_y = curr_y + dy[i]
-        #         if(maze_grid[new_x][new_y] == ".") or (maze_grid[new_x][new_y] == "E") or (maze_grid[new_x][new_y] == "S"):
-        #             new_dir = i
-        #             new_node = (new_x,new_y,new_dir)
-        #             if(new_node not in visited) and (new_dir-curr_dir != 2):
-        #                 dir_diff = (new_dir-curr_dir)%2 
-        #                 weight = 1 if (dir_diff == 0) else dir_diff*1000
-        #                 self.add_edge(curr_node,new_node,weight)
-        #                 queue.append(new_node)                   
-            
+                    self.graph[(x,y)] = {}
         
     def add_edge(self,node1, node2, weight,direction):
         if node1 not in self.graph:
@@ -51,17 +21,17 @@ class Graph:
         self.graph[node1][node2] = (weight,direction)
     
     def shortest_distances(self, source: tuple):
-        distances = {node: (float("inf"),-1) for node in self.graph}
-        distances[source] = 0
+        distances = {node: (float("inf"),-1,[node]) for node in self.graph}
+        distances[source] = (0,1,[(source,1)])
 
-        #((weight,direction),(x,y))
-        pq = [((0,1),source)]
+        #((weight,direction,list_history_nodesDirr),(x,y))
+        pq = [((0,1,[(source,1)]),source)]
         heapify(pq)
         
         visited = set()
         
         while(pq):
-            (current_distance,curr_dir),current_node = heappop(
+            (current_distance,curr_dir,curr_history_nodes),current_node = heappop(
                 pq
             )
             
@@ -85,9 +55,11 @@ class Graph:
             
             for neighbor, (weight, dirr) in self.graph[current_node].items():
                 tentative_distance = current_distance + weight
+                tentative_his_nodes = copy.deepcopy(curr_history_nodes)
+                tentative_his_nodes.append((neighbor,dirr))
                 if tentative_distance < distances[neighbor][0]:
-                    distances[neighbor] = (tentative_distance, dirr)
-                    heappush(pq, ((tentative_distance, dirr), neighbor))
+                    distances[neighbor] = (tentative_distance, dirr, tentative_his_nodes)                    
+                    heappush(pq, ((tentative_distance, dirr, tentative_his_nodes), neighbor))
         
         return distances
 
@@ -102,16 +74,32 @@ def parse_input_file(fileName: str) -> list[list[str]]:
     return maze_grid
 
 if __name__ == "__main__":
-    fileName = "input1.csv"
+    fileName = "test_input1.csv"
     
     maze_grid = parse_input_file(fileName)
-    for line in maze_grid:
-        print(' '.join(line))
+    # for line in maze_grid:
+    #     print(' '.join(line))
     maze_graph = Graph(maze_grid)
-    print()
     
     distances = maze_graph.shortest_distances((1,len(maze_grid)-2))
-    print()
-    to_end = distances[(len(maze_grid)-2,1)]
-    print(to_end)
+
+    to_end = distances[(len(maze_grid)-2,1)]    
+
+    for item in to_end[2]:
+        x,y = item[0]
+        dirr = item[1]
+        obj = maze_grid[y][x]
+        if(obj == "."):
+            if(dirr == 0):
+                maze_grid[y][x] = "^"
+            elif(dirr == 1):
+                maze_grid[y][x] = ">"
+            elif(dirr == 2):
+                maze_grid[y][x] = "v"
+            elif(dirr == 3):
+                maze_grid[y][x] = "<"
     
+    for line in maze_grid:
+        print(' '.join(line))
+    
+    print(f"Total cost: {to_end[0]}")

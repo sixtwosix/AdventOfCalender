0c46f5ca4e66c11427285313414dd5d57dd20fc7
sixtwosix <hencostrauss@gmail.com>
2025-05-05 23:40:08 +0200

    Finished part 2 of day 22

--- Day22/Python/Challenge2.py
+++ Day22/Python/Challenge2.py
@@ -1,7 +1,7 @@
 import sys
-print(sys.getrecursionlimit())
+# print(sys.getrecursionlimit())
 sys.setrecursionlimit(2500)
-print(sys.getrecursionlimit())
+# print(sys.getrecursionlimit())
 
 def parse_file(fileName: str) -> list[int]:
     with open(fileName,'r') as f:
@@ -17,7 +17,7 @@ def get_secret_num_to_depth(init_secret_num: int, maximum_depth_level: int, cach
         if((init_secret_num,depth_level) in cache):
             return cache[(init_secret_num,depth_level)]
         
-        if(depth_level == maximum_depth_level-1):
+        if(depth_level == maximum_depth_level):
             n =  secret_num            
         else:
             new_secret_num = secret_num
@@ -48,45 +48,49 @@ def get_secret_num_to_depth(init_secret_num: int, maximum_depth_level: int, cach
     
     return determine_secret_num(init_secret_num, 0),cache
 
-def determine_stock_market(init_secrets: list[int]) -> list[list[tuple[int,str]]]:   
-    SECRET_NUMBER_COUNT = 10
-     
-    stock_market = [[] for idx in range(len(init_secrets))]
+test_set1 = {}
+
+def determine_stock_market(init_secrets: list[int]) -> dict:   
+    SECRET_NUMBER_COUNT = 2000
+    
+    stock_market = {}
+    stock_cost = [0 for idx in range(len(init_secrets))]
     for idx,secret in enumerate(init_secrets):
         res, cache = get_secret_num_to_depth(secret,SECRET_NUMBER_COUNT,{})
-        temp_prices = [val for key, val in cache.items()][::-1]
+        temp_prices = [val for val in cache.values()][::-1]
+        
         price_diff = []
-        possible_choices = []
         for i,item in enumerate(temp_prices):
             if(i == 0):
-                price_diff.append((i,item,None))
+                price_diff.append((item,None))
             else:
-                price_diff.append((i,item,item-prev_item))
-                if(i > 4):
-                    temp_str = ""
-                    for j in range(3,-1,-1):
-                        temp_str += str(price_diff[i-j][2])
-                    possible_choices.append((item,temp_str))
-            
-            prev_item = item
+                price_diff.append((item,item - price_diff[i-1][0]))
+                if(i >= 3):
+                    temp_str = tuple([price_diff[j][1] for j in range(i-3,i+1)])
+                    if(temp_str not in stock_market):
+                        stock_market[temp_str] = stock_cost.copy()
+                    # monkey will buy the first time he sees the sequence
+                    if(stock_market[temp_str][idx] == 0):
+                        stock_market[temp_str][idx] = item
 
-        
-        possible_choices = sorted(possible_choices, key=lambda x: (x[0], x[1]), reverse=True)
-        # for item_x in possible_choices:
-        #     print(item_x)
-        stock_market[idx] = possible_choices
+        test_set1[(idx)] = price_diff
     
     return stock_market
-    
-def play_stock_market():
-    print()            
+
 
 if __name__ == "__main__":
-    fileName = "test_input3.csv"
+    fileName = "input1.csv"
     init_secrets = parse_file(fileName)
-    print(init_secrets)
     
     stock_market = determine_stock_market(init_secrets)
     
-    for i in range(len(stock_market[0])):
-        print(f"{stock_market[0][i]}\t{stock_market[1][i]}\t{stock_market[2][i]}\t{stock_market[3][i]}")
+    biggest_winner = 0
+    winner_val = []
+    best_stock = ""
+    for key,val in stock_market.items():
+        stock_total_cost = sum(val)
+        if(stock_total_cost >= biggest_winner):
+            biggest_winner = stock_total_cost
+            best_stock = key
+            winner_val = val
+    print(f"The best combo of \"{best_stock}\"\nwill yield {biggest_winner} bananas")

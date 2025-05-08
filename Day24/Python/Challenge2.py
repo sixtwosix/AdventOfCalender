658b4f017a10598525109bbfa6e3d06dbec30551
sixtwosix <hencostrauss@gmail.com>
2025-05-08 17:26:12 +0200

    Day 24 challenge 2 -- completed answer with some manual intervention, would like to change it up to be automated

--- Day24/Python/Challenge2.py
+++ Day24/Python/Challenge2.py
@@ -1,5 +1,5 @@
 import tqdm
-
+import re
 
 
 def parsefile(fileName: str):
@@ -57,27 +57,94 @@ def solve_circuit(circuit: dict):
     
     return circuit
 
+def determine_errors(circuit: dict, memo_circuit: dict, possible_errors):
+    regex_string = r"\[(\w{3}) (XOR|AND|OR) (\w{3})\]"
+    circuit = {v[1]: k for k, v in circuit.items()}
+    memo_circuit = {v: k for k, v in memo_circuit.items()}
+    cache = {}
+    
+    def reverse_engineer(statement: str, statement_memo: str):
+        if((statement,statement_memo) in cache):
+            return cache[(statement,statement_memo)]
+        if(statement == statement_memo):
+            n = True
+            print(f"{statement} -- {statement_memo}")
+        else:
+            res = re.findall(regex_string,statement)
+            res_memo = re.findall(regex_string,statement_memo)
+            
+            if(len(res) == 0 and len(res_memo) == 0):
+                n = False
+                print(f"{statement} -- {statement_memo}")
+            else:
+                for x in res:
+                    x1= " ".join(x)
+                    x2= " ".join(x[::-1])
+                    if(x1 in circuit):
+                        x = x1                
+                    elif(x2 in circuit):
+                        x = x2
+                    y = circuit[x]
+                    x = f"[{x1}]"
+                    statement = statement.replace(x,y)
+                    
+                
+                
+                for x in res_memo:
+                    x1= " ".join(x)
+                    x2= " ".join(x[::-1])
+                    if(x1 in circuit):
+                        x = x1                
+                    elif(x2 in circuit):
+                        x = x2
+                    else:
+                        n = False
+                        cache[(statement,statement_memo)] = n
+                        print(f"{statement} -- {statement_memo}")
+                        return n
+                        
+                    y = circuit[x]
+                    x = f"[{x1}]"
+                    statement_memo = statement_memo.replace(x,y)
+            
+                # print(f"{statement} -- {statement_memo}")
+            
+                n = reverse_engineer(statement,statement_memo)
+        cache[(statement,statement_memo)] = n
+        return n
+    
+    return reverse_engineer(possible_errors[0],possible_errors[1])
+    
+    
 
 if __name__ == "__main__":
     fileName = "input1.csv"
     
     circuit_dict = parsefile(fileName)
+    circuit_dict_original = circuit_dict.copy()
     
     circuit_dict = solve_circuit(circuit_dict)
     
-    # for key in circuit_dict.keys():
-    #     print(f"'{key}' : {circuit_dict[key]}")
-    
-    memo_cache = {}
-    
+    # determine the memo
+    memo_cache = {}    
     for i,(key,val) in enumerate(sorted(circuit_dict.items(),key=lambda x : x[0])):
         if(key[0] == "z" and key[1:].isdigit()):
             count = int(key[1:])
             if(count == 0):
                 memo_cache[f"z{count:0>2}"] = f"[x{count:0>2} XOR y{count:0>2}]"
                 memo_cache[f"c{count:0>2}"] = f"[x{count:0>2} AND y{count:0>2}]"
+            elif(f"x{key[1:]}" not in circuit_dict):
+                memo_cache[f"z{count:0>2}"] = f"{memo_cache[f"c{count-1:0>2}"]}"
             else:
                 memo_cache[f"z{count:0>2}"] = f"[{memo_cache[f"c{count-1:0>2}"]} XOR [x{count:0>2} XOR y{count:0>2}]]"
-                memo_cache[f"c{count:0>2}"] = f"[[x{count:0>2} AND y{count:0>2}] OR [[x{count:0>2} XOR y{count:0>2}] AND {memo_cache[f"c{count-1:0>2}"]}]]"
-            if(val[1] != memo_cache[key]):
-                print(f"{key} : {val[1]} -- {memo_cache[key]}")
+                memo_cache[f"c{count:0>2}"] = f"[[x{count:0>2} AND y{count:0>2}] OR [{memo_cache[f"c{count-1:0>2}"]} AND [x{count:0>2} XOR y{count:0>2}]]]"
+
+    for i,(key,val) in enumerate(sorted(circuit_dict.items(),key=lambda x : x[0])):  
+        if(key[0] == "z" and key[1:].isdigit()):  
+            is_equal = determine_errors(circuit_dict_original,memo_cache,(val[1],memo_cache[key]))
+            if not (is_equal):        
+                print("We have a problem")
+                print("=======================================================================================================================================================")
+                break
+
+    
